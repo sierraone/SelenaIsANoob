179723ec199a25e10ccf82125b593296069068c7
sierraone <yinanwang@projectwhisper.com>
2018-03-21 22:36:16 -0400

    Update GPACalculator.java
    Added scroll panel, copied from test branch.

--- GPACalculator.java
+++ GPACalculator.java
@@ -1,80 +1,39 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.Console;
+import javax.swing.table.DefaultTableModel;
 
-public class c {
-	
-	
+import java.awt.*;
+
+public class GPACalculator {
 	/**
 	 * constructor of calculator
 	 */
-	public c(){
-		initialize(); 
-	}
-
-	/**
-	 * initialization
-	 */
-	public void initialize() {
+	public GPACalculator(){
 		/**
 		 * creating the JFrame
 		 */
-		JFrame f=new JFrame(" BoxLayout tutorial");
-		f.setTitle("GPA Calculator");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(650, 700);
-		f.setLayout(new FlowLayout());
+		JFrame calcFrame=new JFrame("GPA Calculator");
+		calcFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		calcFrame.setSize(650, 700);
+		calcFrame.setLayout(new FlowLayout());
+		JPanel tablePanel=new JPanel();
 
-		JPanel p=new JPanel();
-
-		JTextField course1=new JTextField(7);
-		JTextField credit1=new JTextField(3);
-		JTextField grade1=new JTextField(3);
-		JButton reset1=new JButton("Reset");
-
-		JTextField course2=new JTextField(7);
-		JTextField credit2=new JTextField(3);
-		JTextField grade2=new JTextField(3);
-		JButton reset2=new JButton("Reset");
-
-		//table
-		String[] columnNames= {"Course","Credit Hour","Grade", "  "};
-		Object [][] data= {{course1,credit1,grade1,reset1},
-				{course2,credit2,grade2,reset2}
-		};
+		/**
+		 * Jtable
+		 */
 		
-		System.out.println(course1);
-		JTable table=new JTable(data,columnNames);
-		table.setPreferredScrollableViewportSize(new Dimension(500, 70));
-		table.setFillsViewportHeight(true);
-		p.add(table);
-
-
+		String[] colHeadings = {"Course","Credit","Grade"," Reset Row"};
+		int numRows = 1;
+		DefaultTableModel model = new DefaultTableModel(numRows, colHeadings.length) ;
+		model.setColumnIdentifiers(colHeadings);
+		JTable calcTable = new JTable(model);
+		JScrollPane scrollTable = new JScrollPane(calcTable);
+		tablePanel.add(scrollTable);
 		/**
 		 * grade arrays
 		 */
 		String[] gradeoptions = {"A+","A","A-","B+","B","B-","C+","C","C-",
 				"D+","D","D-"};
 		double[] gradeoptions2= {4.0,4.0,3.7,3.3,3,2.7,2.3,2,1.7,1.3,1,0.7,0};
-
-
-		/**
-		 *reset button1
-		 */	
-		reset1.addActionListener(new ActionListener()
-		{
-			@Override
-			public void actionPerformed(ActionEvent event)
-			{
-				course1.setText(" ");
-				grade1.setText(" ");
-				credit1.setText(" ");
-
-			}
-		});
-
 		
 		/**
 		 * panel at the bottom
@@ -88,8 +47,14 @@ public class c {
 		JLabel requiredgpa = new JLabel("Required GPA:      ");
 		String note="";
 		JLabel a = new JLabel("Advice:  "+note);
+		JButton reset=new JButton("Reset all");
+		JButton a15=new JButton("Add 15 Courses");
+		JButton a1=new JButton("Add 1 course");
+		JButton d1=new JButton("Delete 1 course");
+		
+		
 		/**
-		 * adding labels for bottom
+		 * adding labels,textfield, and button to pbottom
 		 */
 		pbottom.add(calculate);
 		pbottom.add(currentgpa);
@@ -97,24 +62,26 @@ public class c {
 		pbottom.add(target);
 		pbottom.add(requiredgpa);
 		pbottom.add(a);
+		pbottom.add(reset);
+		pbottom.add(a15);
+		pbottom.add(a1);
+		pbottom.add(d1);
 
 		/**
 		 * add panel 1 and panel 2 to the frame
 		 */
-		f.add(p,BorderLayout.NORTH);
-		f.add(pbottom,BorderLayout.SOUTH);
-		f.setVisible(true);
-		
-		
+		calcFrame.add(tablePanel,BorderLayout.NORTH);
+		calcFrame.add(pbottom,BorderLayout.SOUTH);
+		calcFrame.setVisible(true);
 	}
+
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				new c();
+				new GPACalculator();
 			}
 		});
 	}
-	
 
 
 }
