7d7f0a6fd2b64d5b62d4555b30bf402011f6294d
sierraone <yinanwang@projectwhisper.com>
2018-03-21 23:12:11 -0400

    Update GPACalculator.java
    WIP

--- GPACalculator.java
+++ GPACalculator.java
@@ -1,78 +1,106 @@
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GPACalculator {
 	/**
 	 * constructor of calculator
 	 */
 	public GPACalculator(){
+		initialize(); 
+	}
+
+	/**
+	 * initialization
+	 */
+	public void initialize() {
 		/**
 		 * creating the JFrame
 		 */
-		JFrame calcFrame=new JFrame("GPA Calculator");
-		calcFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		calcFrame.setSize(650, 700);
-		calcFrame.setLayout(new FlowLayout());
-		JPanel tablePanel=new JPanel();
+		JFrame frame=new JFrame("GPA Calculator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(650, 700);
+		frame.setLayout(new BorderLayout());
 
 		/**
-		 * Jtable
+		 * panels
 		 */
-		
-		String[] colHeadings = {"Course","Credit","Grade"," Reset Row"};
-		int numRows = 1;
+		JPanel panel1=new JPanel();
+		JPanel panel2=new JPanel();
+		JPanel panel3=new JPanel();
+		panel1.setLayout(new BoxLayout(panel1, BoxLayout.X_AXIS));
+		panel2.setLayout(new BoxLayout(panel2, BoxLayout.Y_AXIS));
+		panel3.setLayout(new BoxLayout(panel3, BoxLayout.Y_AXIS));
+
+		/**
+		 * JTable
+		 */
+		String[] colHeadings = {"Course","Credit","Grade"};
+		int numRows = 5 ;
 		DefaultTableModel model = new DefaultTableModel(numRows, colHeadings.length) ;
 		model.setColumnIdentifiers(colHeadings);
-		JTable calcTable = new JTable(model);
-		JScrollPane scrollTable = new JScrollPane(calcTable);
-		tablePanel.add(scrollTable);
+		JTable table = new JTable(model);
+		JScrollPane scroll = new JScrollPane(table);
+		panel1.add(scroll);
+
 		/**
 		 * grade arrays
 		 */
 		String[] gradeoptions = {"A+","A","A-","B+","B","B-","C+","C","C-",
 				"D+","D","D-"};
 		double[] gradeoptions2= {4.0,4.0,3.7,3.3,3,2.7,2.3,2,1.7,1.3,1,0.7,0};
-		
-		/**
-		 * panel at the bottom
-		 */
-		JPanel pbottom=new JPanel();
-		pbottom.setLayout(new BoxLayout(pbottom, BoxLayout.LINE_AXIS ));
-		JButton calculate=new JButton("Calculate");
-		JLabel currentgpa = new JLabel("Current GPA:      ");
-		JLabel targetgpa = new JLabel("Target GPA: ");
-		JTextField target=new JTextField(3);
-		JLabel requiredgpa = new JLabel("Required GPA:      ");
-		String note="";
-		JLabel a = new JLabel("Advice:  "+note);
-		JButton reset=new JButton("Reset all");
-		JButton a15=new JButton("Add 15 Courses");
-		JButton a1=new JButton("Add 1 course");
-		JButton d1=new JButton("Delete 1 course");
-		
-		
-		/**
-		 * adding labels,textfield, and button to pbottom
-		 */
-		pbottom.add(calculate);
-		pbottom.add(currentgpa);
-		pbottom.add(targetgpa);
-		pbottom.add(target);
-		pbottom.add(requiredgpa);
-		pbottom.add(a);
-		pbottom.add(reset);
-		pbottom.add(a15);
-		pbottom.add(a1);
-		pbottom.add(d1);
 
 		/**
-		 * add panel 1 and panel 2 to the frame
+		 * label
 		 */
-		calcFrame.add(tablePanel,BorderLayout.NORTH);
-		calcFrame.add(pbottom,BorderLayout.SOUTH);
-		calcFrame.setVisible(true);
+		JLabel currentgpa = new JLabel("Current GPA:");
+		JLabel targetgpa = new JLabel("Target GPA:");
+		JLabel requiredgpa = new JLabel("Required GPA:");
+		String note="";
+
+		/**
+		 * test field
+		 */
+		JTextField target=new JTextField(5);
+
+		/**
+		 * buttons
+		 */
+		JButton calculate=new JButton("Calculate");
+		JButton reset=new JButton("Reset all");
+		JButton quickadd=new JButton("Quick Add");
+		JButton addrow=new JButton("Add Row");
+		JButton deleterow=new JButton("Delete Row");
+
+		/**
+		 * adding labels,textfield, and button to panel2
+		 */
+		panel2.add(calculate);
+		panel2.add(reset);
+		panel2.add(quickadd);
+		panel2.add(addrow);
+		panel2.add(deleterow);
+		
+		/**
+		 * comments
+		 */
+		String comment="";
+		JLabel comments= new JLabel("Advice:  "+note);
+		panel3.add(comments);
+		panel3.add(currentgpa);
+		panel3.add(targetgpa);
+		panel3.add(target);
+		panel3.add(requiredgpa);
+
+		/**
+		 * add panel1 and panel2 to the frame
+		 */
+		panel1.add(panel2);
+		frame.add(panel1, BorderLayout.PAGE_START);
+		frame.add(panel3, BorderLayout.CENTER);
+		frame.setVisible(true);
 	}
 
 	public static void main(String[] args) {
