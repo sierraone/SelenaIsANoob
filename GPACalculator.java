b72830065e36feb7a7c9d02361606a7ddb2b0ba1
sierraone <yinanwang@projectwhisper.com>
2018-03-21 22:45:01 -0400

    Update GPACalculator.java

--- GPACalculator.java
+++ GPACalculator.java
@@ -24,8 +24,13 @@ public class GPACalculator {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(650, 700);
 		frame.setLayout(new FlowLayout());
-		JPanel panel1=new JPanel();
 
+		/**
+		 * panels
+		 */
+		JPanel panel1=new JPanel();
+		JPanel panel2=new JPanel();
+		panel2.setLayout(new BoxLayout(panel2, BoxLayout.LINE_AXIS ));
 
 		/**
 		 * Jtable
@@ -44,25 +49,30 @@ public class GPACalculator {
 		String[] gradeoptions = {"A+","A","A-","B+","B","B-","C+","C","C-",
 				"D+","D","D-"};
 		double[] gradeoptions2= {4.0,4.0,3.7,3.3,3,2.7,2.3,2,1.7,1.3,1,0.7,0};
-		
+
 		/**
-		 * panel at the bottom
+		 * label
 		 */
-		JPanel panel2=new JPanel();
-		panel2.setLayout(new BoxLayout(panel2, BoxLayout.LINE_AXIS ));
-		JButton calculate=new JButton("Calculate");
 		JLabel currentgpa = new JLabel("Current GPA:      ");
 		JLabel targetgpa = new JLabel("Target GPA: ");
-		JTextField target=new JTextField(3);
 		JLabel requiredgpa = new JLabel("Required GPA:      ");
 		String note="";
 		JLabel a = new JLabel("Advice:  "+note);
+
+		/**
+		 * test field
+		 */
+		JTextField target=new JTextField(3);
+
+		/**
+		 * buttons
+		 */
+		JButton calculate=new JButton("Calculate");
 		JButton reset=new JButton("Reset all");
-		JButton a15=new JButton("Add 15 Courses");
-		JButton a1=new JButton("Add 1 course");
-		JButton d1=new JButton("Delete 1 course");
-		
-		
+		JButton quickadd=new JButton("Quick Add");
+		JButton addrow=new JButton("Add Row");
+		JButton deleterow=new JButton("Delete Row");
+
 		/**
 		 * adding labels,textfield, and button to pbottom
 		 */
@@ -73,9 +83,9 @@ public class GPACalculator {
 		panel2.add(requiredgpa);
 		panel2.add(a);
 		panel2.add(reset);
-		panel2.add(a15);
-		panel2.add(a1);
-		panel2.add(d1);
+		panel2.add(quickadd);
+		panel2.add(addrow);
+		panel2.add(deleterow);
 
 		/**
 		 * add panel 1 and panel 2 to the frame
